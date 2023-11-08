3f104d2e30c5e4351d79d784776845b01961ee30
jarray-kais <jarraykais1@gmail.com>
2023-11-08 16:29:40 +0100

    react

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{Component} from 'react'
+//import Home from './home';
+
+class App extends Component{
+  state={
+    
+  }
+
+  render(){
+    return <div>
+   <h1>welcome to reactjs course</h1>
+      {/*<Home firstname="kais" lastname ="jarray" age="30"/>*/}
+
+    </div>
+  }
+}
+
+e
---
+++ app/src/home.js
@@ -0,0 +1,16 @@
+// import React,{Component} from 'react'
+
+
+// class Home extends Component{
+
+
+//     render(){
+//         const {firstname , lastname , age} = this.props ;
+//         return <div>
+//             welcome to home page Component <br />
+//             my name is {lastname} {firstname} I'am {age} years old
+
+//         </div>
+//     }
+// }
+// export default Home;
