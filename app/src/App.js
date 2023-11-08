d7d5989ac270fe04ca337cc2752c0967f0670478
jarray-kais <jarraykais1@gmail.com>
2023-11-08 18:14:20 +0100

    change

--- app/src/App.js
+++ app/src/App.js
@@ -5,16 +5,47 @@ import React,{Component} from 'react'
 
 class App extends Component{
   state={
-    
+    profile:[
+      {id:1,name:"kais", lastname:"jarray",age:29},
+      {id:2,name:"houssem", lastname:"bjaoui",age:25},
+      {id:3,name:"bilel", lastname:"bjaoui",age:31},
+      {id:4,name:"ali", lastname:"terti", age:40},
+    ]
+
   }
 
   render(){
-    return <div>
+    const data=this.state.profile.map((item)=>{
+      return (
+        <ul key={item.id}>
+          <li>{item.name}</li>
+          <li>{item.lastname}</li>
+          <li>{item.age}</li>
+        </ul>
+      )
+    })
+    const age=this.state.profile.map((item)=>{
+      if(item.age<30){
+        return <ul key={item.id}>
+          <li>{item.name}</li>
+          <li>{item.lastname}</li>
+          <li>{item.age}</li>
+        </ul>
+      }
+    })
+    return <div className='App'>
    <h1>welcome to reactjs course</h1>
       {/*<Home firstname="kais" lastname ="jarray" age="30"/>*/}
-
+      <div>{data}</div>
+      <div>
+        <h1>age inferieur a 30 </h1>
+        {age}
+      </div>
+      <div><ul>
+        {this.state.profile.map((item,index)=><li>{index}-{item.name}</li>)}
+      </ul></div>
     </div>
   }
 }
 
-e
+export default App;
